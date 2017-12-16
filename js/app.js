1bce902eb5e9199184a7d8d52ae4cb169a55ed48
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-16 11:56:27 -0600

    Commit 2 - It's all about me done

--- js/app.js
+++ js/app.js
@@ -7,3 +7,30 @@ let clicker = {
 }
 
 clicker.click();
+
+
+let myInformation = {
+	name: "Jackson",
+	age: 24,
+	funFact: "I was born in Australia",
+	anotherYear: function () {
+		return this.age += 1
+	},
+	agePlus: function () {
+		return this.age + 10
+	},
+	newName: function (string) {
+		return this.name = string;
+	},
+	statement: function () {
+		console.log("Hello, my name is " + this.name + " and I am " + this.age + ". Here is one fun fact about me. " + this.funFact + ". In ten years I will be " + this.agePlus());
+	},
+}
+
+// console.log(myInformation.agePlus());
+
+myInformation.anotherYear();
+
+myInformation.newName("Rumpelstiltskin");
+
+console.log(myInformation);
