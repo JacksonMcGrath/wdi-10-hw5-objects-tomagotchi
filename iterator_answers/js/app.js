c7df00b81ff93b728208f2ad5b805676e190da69
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-17 15:34:36 -0600

    Commit 8 - forEach done

---
+++ iterator_answers/js/app.js
@@ -0,0 +1,10 @@
+const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+
+// let capitalized = [];
+//capitalize each word and console.log them
+words.forEach((str) => {
+	// set a new variable to hold the capitalized strings
+	let capitalized = str.toUpperCase()
+	console.log(capitalized);
+});
+
