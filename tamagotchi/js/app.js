3610eb5d9d074a436fd03fb884bb76dc5d65db5e
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-17 18:49:38 -0600

    Commit 16 - made a 2nd Tamogatchi

--- tamagotchi/js/app.js
+++ tamagotchi/js/app.js
@@ -18,3 +18,24 @@ let tamagotchi1 = {
   		console.log(this.name + " let out a yawn... Yaaaaaaawn");
   	},
 }
+
+let tamagotchi2 = {
+	name: "Nymeria",
+  	creatureType: "Wolf",
+  	foodInTummy: 10,
+  	restedness: 10,
+  	health: 10,
+  	cry: function () {
+  		console.log("Waaaaa");
+  		this.foodInTummy -= 1;
+  		console.log(this.name + "'s foodInTummy is now " + this.foodInTummy);
+  	},
+  	puke: function () {
+  		console.log(this.name + " threw up... Blaaaauuugh!");
+  		this.foodInTummy -= 1;
+  		console.log(this.name + "'s foodInTummy is now " + this.foodInTummy);
+  	},
+  	yawn: function () {
+  		console.log(this.name + " let out a yawn... Yaaaaaaawn");
+  	},
+}
