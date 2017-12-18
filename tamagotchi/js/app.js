cf4363c6b9a2133a3b5de5228ae943a7b0fff115
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-17 18:47:06 -0600

    Commit 15 - updated puke and yawn methods

--- tamagotchi/js/app.js
+++ tamagotchi/js/app.js
@@ -6,11 +6,15 @@ let tamagotchi1 = {
   	health: 10,
   	cry: function () {
   		console.log("Waaaaa");
+  		this.foodInTummy -= 1;
+  		console.log(this.name + "'s foodInTummy is now " + this.foodInTummy);
   	},
   	puke: function () {
-  		console.log("Blaaaauuugh!");
+  		console.log(this.name + " threw up... Blaaaauuugh!");
+  		this.foodInTummy -= 1;
+  		console.log(this.name + "'s foodInTummy is now " + this.foodInTummy);
   	},
   	yawn: function () {
-  		console.log("Yaaaaaaawn");
+  		console.log(this.name + " let out a yawn... Yaaaaaaawn");
   	},
 }
